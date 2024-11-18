77d29e036674262c021584a763e46b9e5f42e189
JoshuaRussell <50185858+JustKarkat@users.noreply.github.com>
2024-11-18 03:04:53 +0000

    Fully add selectable Joke type

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
 	"time"
 )
+
+// Joke Types
+type joke_types []string
 
 // Joke Struct
 type Joke struct {
@@ -24,18 +29,59 @@ type Joke struct {
 
 func main() {
 	fmt.Println("Getting your joke...")
-	url := "https://official-joke-api.appspot.com/random_joke"
+	url := "https://official-joke-api.appspot.com/jokes/random/1"
+	jokeTypeURL := "https://official-joke-api.appspot.com/types"
+	// use default chrome user agent for Windows 10.
+	userAgent := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+
 	jokeClient := http.Client{
 		Timeout: time.Second * 5, // Timeout after 5 seconds
 	}
 	fmt.Print("\033[H\033[2J") // clear console
+
+	// get joke types
+	typereq, err := http.NewRequest("GET", jokeTypeURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	typereq.Header.Set("User-Agent", userAgent)
+
+	typeres, getErr := jokeClient.Do(typereq)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	if typeres.Body != nil {
+		defer typeres.Body.Close()
+	}
+	typebody, readErr := io.ReadAll(typeres.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	types := joke_types{}
+	jsonErrtyp := json.Unmarshal(typebody, &types)
+	if jsonErrtyp != nil {
+		log.Fatal(jsonErrtyp)
+	}
+
+	fmt.Println("What joke type?" + "\n" + strings.Join(types[:], ","))
+	selected_type := bufio.NewScanner(os.Stdin)
+	selected_type.Scan()
+
+	selectionExists := (slices.Contains(types, selected_type.Text()))
+
+	// Get Jokes
+	// add joke type.
+	if !(selected_type.Err() != nil) && (len(selected_type.Text()) > 0) && selectionExists {
+		url = "https://official-joke-api.appspot.com/jokes/" + selected_type.Text() + "/random/"
+	}
+
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
-	// use default chrome user agent for Windows 10.
+	req.Header.Set("User-Agent", userAgent)
 
 	res, getErr := jokeClient.Do(req)
 	if getErr != nil {
@@ -51,15 +97,21 @@ func main() {
 		log.Fatal(readErr)
 	}
 
-	joke1 := Joke{}
-	jsonErr := json.Unmarshal(body, &joke1)
+	jokes := []Joke{}
+	jsonErr := json.Unmarshal(body, &jokes)
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
 
-	fmt.Println("Heres your joke!" + "\n" + joke1.Setup)
-	fmt.Println("Enter for punchline...")
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-	fmt.Println(joke1.Punchline)
+	if len(jokes) <= 0 {
+		fmt.Println("Failed to get your joke :( !!!")
+		fmt.Println("API RESPONSE: " + "\n" + string(body))
+	} else {
+		fmt.Println("Heres your joke!" + "\n" + jokes[0].Setup)
+		fmt.Println("Enter for punchline...")
+		input := bufio.NewScanner(os.Stdin)
+		input.Scan()
+		fmt.Println(jokes[0].Punchline)
+	}
+
 }
